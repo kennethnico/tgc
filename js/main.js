7ffaabc2c9034e4f9e5530d70c1f35e6ba029e47
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-08-09 14:20:34 -0600

    Construyendo agregando recursos y verificando funcionamiento

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 let map = L.map('map').setView(41.587148, -85.835456,6);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '© OpenStreetMap'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 //Marcadores
-var mrkr1 = L.marker([51.5, -0.09]).addTo(map);
+var mrkr1 = L.marker([41.58423821413766, -85.83468629014831]).addTo(map);
+var mrkr2 = L.marker([41.5812928369633, -85.83179183847852]).addTo(map);
+var mrkr3 = L.marker([41.57948729254225, -85.82937809055288]).addTo(map);
