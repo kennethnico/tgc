b5b62028554cf526b483f5d208933eab2aa65373
kenethnic <79937993+kennethnico@users.noreply.github.com>
2023-08-23 11:18:25 -0600

    Mapa - Añandiendo Lugares/Pines

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
-let map = L.map('map').setView([41.58098170748805, -85.82310817483332],12);
+let map = L.map('map').setView([41.58472821232003, -85.83402024107362],15);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; OpenStreetMap contributors'}).addTo(map);
 //Pines
-const restaurantIcon = L.icon({
+const mainIcon = L.icon({
     iconUrl: '../assets/img/icon_pins/main_Loc.png',
-    iconSize: [38, 95], // size of the icon
+    iconSize: [50, 50], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+const restaurantIcon = L.icon({
+    iconUrl: '../assets/img/icon_pins/restaurant.png',
+    iconSize: [45, 50], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+const hospitalIcon = L.icon({
+    iconUrl: '../assets/img/icon_pins/hospital.png',
+    iconSize: [50, 50], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+const parkIcon = L.icon({
+    iconUrl: '../assets/img/icon_pins/park.png',
+    iconSize: [50, 50], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+const placesIcon = L.icon({
+    iconUrl: '../assets/img/icon_pins/places.png',
+    iconSize: [50, 50], // size of the icon
     iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
@@ -11,5 +35,13 @@ const restaurantIcon = L.icon({
 var mrkr1 = L.marker([41.58423821413766, -85.83468629014831]).addTo(map);
 mrkr1.bindPopup("<div class='text-center'><b>Sitio de interés</b><br>Nombre.<br><img class='img-thumbnail' src='../assets/img/cta-bg.jpg' alt='Prueba de imagen'><br><a href='https://www.youtube.com/channel/UCpPMJtSltAmFs_oamw7jEig' target='_blank'>Ver mas...</a></div>");
 var mrkr2 = L.marker([41.5812928369633, -85.83179183847852]).addTo(map);
+mrkr2.bindTooltip("Tooltip");
 var mrkr3 = L.marker([41.57948729254225, -85.82937809055288]).addTo(map);
-let mainMark = L.marker([41.58472821232003, -85.83402024107362],{icon:restaurantIcon}).addTo(map);
+let mainMark = L.marker([41.58472821232003, -85.83402024107362],{icon:mainIcon}).addTo(map);
+mainMark.bindTooltip("The Goshen Chronicles");
+let restaurantMark = L.marker([41.58682178463862, -85.83352039098216],{icon:restaurantIcon}).addTo(map);
+let hospitalMark = L.marker([41.58188682922779, -85.83396473695191],{icon:hospitalIcon}).addTo(map);
+hospitalMark.bindPopup("<div class='text-center'><b>Farmacia</b><br>CVS Pharmacy<br><img class='img-thumbnail' src='https://tpc.googlesyndication.com/simgad/4746028788871522326' alt='Prueba de imagen'><br><a href='https://www.cvs.com/minuteclinic/virtual-care/telehealth-options?WT.mc_id=LS_CVSRX_GOOGLE_6472_get_online_care_button' target='_blank'>Ver mas...</a></div>");
+let parkMark = L.marker([41.587892377488906, -85.84174544989568],{icon:parkIcon}).addTo(map);
+parkMark.bindTooltip("Rogers Park");
+let placesMark = L.marker([41.58472821232003, -85.83402024107362],{icon:placesIcon}).addTo(map);
